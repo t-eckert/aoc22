12eee4c1e829f9078421171c32cdbbda127ce545
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-12-17 17:26:02 -0500

    Finish day 12

--- 12/main.go
+++ 12/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/TwiN/go-color"
 )
 
 type Pos struct {
 	X int
 	Y int
+}
+
+func eq(a, b Pos) bool {
+	return (a.X == b.X) && (a.Y == b.Y)
 }
 
 func main() {
@@ -18,36 +20,39 @@ func main() {
 		panic(err.Error())
 	}
 
-	terrain := parseTerrain(raw)
-	domain := search(terrain, newVisited(terrain), [][]Pos{{start(terrain)}}, goal(terrain))
-	fmt.Println(domain)
+	terrain, start, goal := parseTerrain(raw)
+	shortest := search(terrain, []Pos{start}, goal)
+	fmt.Println(shortest)
+
+	shortest2 := search(terrain, getZeroes(terrain), goal)
+	fmt.Println(shortest2)
+
 }
 
-func parseTerrain(raw []byte) [][]int {
-	terrain := [][]int{{}}
+func parseTerrain(raw []byte) (terrain [][]int, start, goal Pos) {
+	terrain = append(terrain, []int{})
 
-	row := 0
+	x, y := 0, 0
 	for _, c := range raw[:len(raw)-1] {
 		if c == '\n' {
-			row++
+			y++
+			x = 0
 			terrain = append(terrain, []int{})
 		} else {
-			terrain[row] = append(terrain[row], height(c))
+			if c == 'S' {
+				start = Pos{x, y}
+				terrain[y] = append(terrain[y], 0)
+			} else if c == 'E' {
+				goal = Pos{x, y}
+				terrain[y] = append(terrain[y], 25)
+			} else {
+				terrain[y] = append(terrain[y], height(c))
+			}
+			x++
 		}
 	}
 
-	return terrain
-}
-
-func newVisited(terrain [][]int) [][]int {
-	width, height := len(terrain[0]), len(terrain)
-
-	visited := make([][]int, height)
-	for i := 0; i < height; i++ {
-		visited[i] = make([]int, width)
-	}
-
-	return visited
+	return terrain, start, goal
 }
 
 func height(c byte) int {
@@ -58,134 +63,72 @@ func char(i int) byte {
 	return byte(i + 97)
 }
 
-func posOf(terrain [][]int, value int) Pos {
-	for y := range terrain {
-		for x := range terrain[y] {
-			if terrain[y][x] == value {
-				return Pos{x, y}
+func search(terrain [][]int, domain []Pos, goal Pos) int {
+	shortest := len(terrain) * len(terrain[0])
+
+	for _, pos := range domain {
+		current := len(terrain) * len(terrain[0])
+		distances := map[Pos]int{pos: 0}
+		toVisit := []Pos{pos}
+
+		for 0 < len(toVisit) {
+			this := toVisit[0]
+			toVisit = toVisit[1:]
+
+			if eq(this, goal) && distances[this] < current {
+				current = distances[this]
+			}
+
+			for _, neighbor := range neighbors(this, terrain) {
+				_, visited := distances[neighbor]
+
+				if !visited {
+					toVisit = append(toVisit, neighbor)
+					distances[neighbor] = distances[this] + 1
+				}
 			}
 		}
-	}
-	return Pos{-1, -1}
-}
 
-func start(terrain [][]int) Pos {
-	return posOf(terrain, -14)
-}
-
-func goal(terrain [][]int) Pos {
-	return posOf(terrain, -28)
-}
-
-func contains(values []int, val int) bool {
-	for _, value := range values {
-		if value == val {
-			return true
+		if current < shortest {
+			shortest = current
 		}
 	}
 
-	return false
+	return shortest
 }
 
-func search(terrain, visited [][]int, domain [][]Pos, goal Pos) [][]Pos {
-	for _, pos := range domain[len(domain)-1] {
-		visited[pos.Y][pos.X] = 1
-	}
+func neighbors(pos Pos, terrain [][]int) []Pos {
+	neighbors := make([]Pos, 0, 4)
 
-	if contains(subset(terrain, domain[len(domain)-1]), -28) {
-		return domain
-	}
-
-	printTerrain(terrain, visited)
-
-	next := next(terrain, visited, domain[len(domain)-1])
-	return search(terrain, visited, append(domain, next), goal)
-}
-
-func next(terrain, visited [][]int, current []Pos) []Pos {
-	next := []Pos{}
-
-	return next
-}
-
-func canGo(terrain [][]int, from, to Pos) bool {
-	fromHeight := terrain[from.Y][from.X]
-	toHeight := terrain[to.Y][to.X]
-
-	// Start and end cases
-	if fromHeight == -14 && toHeight < 2 {
-		return true
-	}
-	if toHeight == -28 && fromHeight == 24 {
-		return true
-	}
-
-	return toHeight-fromHeight < 2
-}
-
-func neighbors(pos Pos) []Pos {
-	return []Pos{
-		{pos.X + 1, pos.Y + 1},
-		{pos.X + 1, pos.Y - 1},
-		{pos.X - 1, pos.Y + 1},
-		{pos.X - 1, pos.Y - 1},
+	for _, neighbor := range []Pos{
 		{pos.X + 1, pos.Y},
 		{pos.X - 1, pos.Y},
 		{pos.X, pos.Y + 1},
 		{pos.X, pos.Y - 1},
-	}
-}
-
-func filterNeighbors(neighbors []Pos, visited [][]int) []Pos {
-	filtered := []Pos{}
-	for _, neighbor := range neighbors {
-		if neighbor.X < 0 || len(visited[0])-1 < neighbor.X {
+	} {
+		if neighbor.X < 0 || len(terrain[0])-1 < neighbor.X {
 			continue
 		}
-		if neighbor.Y < 0 || len(visited)-1 < neighbor.Y {
+		if neighbor.Y < 0 || len(terrain)-1 < neighbor.Y {
 			continue
 		}
-		if visited[neighbor.Y][neighbor.X] == 1 {
-			continue
+		if terrain[neighbor.Y][neighbor.X]-terrain[pos.Y][pos.X] <= 1 {
+			neighbors = append(neighbors, neighbor)
 		}
-		filtered = append(filtered, neighbor)
-	}
-	return filtered
-}
-
-func subset(terrain [][]int, domain []Pos) []int {
-	subset := make([]int, len(domain))
-	for i, pos := range domain {
-		subset[i] = terrain[pos.Y][pos.X]
 	}
 
-	return subset
+	return neighbors
 }
 
-func path(domains [][]Pos) []Pos {
-	path := []Pos{}
-
-	return path
-}
-
-func printTerrain(terrain, visited [][]int) {
-	fmt.Print("\033[H\033[2J")
+func getZeroes(terrain [][]int) []Pos {
+	zeroes := []Pos{}
 	for y, row := range terrain {
-		for x, col := range row {
-			switch col {
-			case -14:
-				fmt.Print(color.InBold(color.InGreenOverWhite("S")))
-			case -28:
-				fmt.Print(color.InBold(color.InRedOverWhite("E")))
-			default:
-				if visited[y][x] == 1 {
-					fmt.Print(color.InCyan(string(char(col))))
-				} else {
-					fmt.Print(color.InWhite(string(char(col))))
-				}
+		for x, value := range row {
+			if value == 0 {
+				zeroes = append(zeroes, Pos{x, y})
 			}
-
 		}
-		fmt.Println()
 	}
+
+	return zeroes
 }
